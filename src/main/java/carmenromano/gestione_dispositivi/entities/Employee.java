b34d1b44ce9a525b57bd3c28123d6400c2d57c83
Carmen Romano <my-9@hotmail.it>
2024-06-28 12:03:59 +0200

    aggiunte classi,relazioni e gestione errori

---
+++ src/main/java/carmenromano/gestione_dispositivi/entities/Employee.java
@@ -0,0 +1,39 @@
+package carmenromano.gestione_dispositivi.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Device> deviceList;
+}
